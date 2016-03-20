7caaad430c99afb33849d807af43aa1cc434af0f
Vincent Klaiber <vincentklaiber@gmail.com>
2016-03-20 17:38:14 +0100

    Add uuid and update defaults

--- plate.php
+++ plate.php
@@ -20,12 +20,13 @@
 
 add_action('after_setup_theme', function () {
     require_if_theme_supports('plate-dashboard', __DIR__.'/src/dashboard.php');
-    require_if_theme_supports('plate-defaults', __DIR__.'/src/defaults.php');
     require_if_theme_supports('plate-editor', __DIR__.'/src/editor.php');
     require_if_theme_supports('plate-footer', __DIR__.'/src/footer.php');
     require_if_theme_supports('plate-login', __DIR__.'/src/login.php');
     require_if_theme_supports('plate-menu', __DIR__.'/src/menu.php');
     require_if_theme_supports('plate-permalink', __DIR__.'/src/permalink.php');
     require_if_theme_supports('plate-tabs', __DIR__.'/src/tabs.php');
+    require_if_theme_supports('plate-theme', __DIR__.'/src/theme.php');
     require_if_theme_supports('plate-toolbar', __DIR__.'/src/toolbar.php');
+    require_if_theme_supports('plate-uuid', __DIR__.'/src/uuid.php');
 }, 100);
