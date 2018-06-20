1e3aebc34c1a875d02c71209375e6652258d3fc1
chris andersson <chris@puredazzle.se>
2018-06-20 13:00:09 +0200

    Updated styles for file

--- src/disable-api.php
+++ src/disable-api.php
@@ -12,14 +12,14 @@
 declare(strict_types=1);
 
 // Require authentication for all requests for the REST API.
-add_filter('rest_authentication_errors', function($result) {
+add_filter('rest_authentication_errors', function ($result) {
     if (!empty($result)) {
         return $result;
     }
-    
+
     if (!is_user_logged_in()) {
         return new WP_Error('rest_not_logged_in', 'You are not currently logged in.', ['status' => 401]);
     }
-    
+
     return $result;
 });
