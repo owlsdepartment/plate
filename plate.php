a69acad5e3fba5af4854599128f62c30350deda9
Vincent Klaiber <vincent@doubledip.se>
2020-01-27 21:21:32 +0100

    Update comments

--- plate.php
+++ plate.php
@@ -1,12 +1,12 @@
 <?php
 
-/*
- * This file is part of WordPlate.
- *
- * (c) Vincent Klaiber <hello@doubledip.se>
+/**
+ * Copyright (c) Vincent Klaiber.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * @see https://github.com/wordplate/plate
  */
 
 /*
@@ -15,16 +15,16 @@
  * Author: WordPlate
  * Author URI: https://wordplate.github.io/
  * Version: 6.0.1
- * Plugin URI: https://github.com/wordplate/plate#readme
+ * Plugin URI: https://github.com/wordplate/plate
  */
 
 declare(strict_types=1);
 
 add_action('after_setup_theme', function () {
-    require_if_theme_supports('plate-disable-dashboard', __DIR__.'/src/disable-dashboard.php');
-    require_if_theme_supports('plate-disable-menu', __DIR__.'/src/disable-menu.php');
-    require_if_theme_supports('plate-disable-toolbar', __DIR__.'/src/disable-toolbar.php');
-    require_if_theme_supports('plate-footer-text', __DIR__.'/src/footer-text.php');
-    require_if_theme_supports('plate-login-logo', __DIR__.'/src/login-logo.php');
-    require_if_theme_supports('plate-permalink', __DIR__.'/src/permalink.php');
+    require_if_theme_supports('plate-disable-dashboard', __DIR__ . '/src/disable-dashboard.php');
+    require_if_theme_supports('plate-disable-menu', __DIR__ . '/src/disable-menu.php');
+    require_if_theme_supports('plate-disable-toolbar', __DIR__ . '/src/disable-toolbar.php');
+    require_if_theme_supports('plate-footer-text', __DIR__ . '/src/footer-text.php');
+    require_if_theme_supports('plate-login-logo', __DIR__ . '/src/login-logo.php');
+    require_if_theme_supports('plate-permalink', __DIR__ . '/src/permalink.php');
 }, 100);
